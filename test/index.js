f23cc39ae67fe7eabd8c349fbd7607e5e6b084cf
zxjun <715213471@qq.com>
2017-02-13 15:30:20 +0800

    init

--- test/index.js
+++ test/index.js
@@ -5,10 +5,9 @@
 const Aspect = require('../lib');
 let aspect = new Aspect('E:/**/node-aop/**/*.js');
 aspect.before(() => {
-    console.log('before', [...arguments].slice(0, 1));
-}).around((target, bean, args) => {
-    console.log('around');
-    return Reflect.apply(target, bean, args);
+    console.log('before');
+}).before(() => {
+    console.log('before2');
 }).aop();
 
 const Hello = require('./Hello');
